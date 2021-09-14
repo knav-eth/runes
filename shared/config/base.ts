93f42d1e1e3210451e55093d63b2117f7b2db410
Knav <me@knav.link>
2021-09-14 16:53:58 -0700

    Cleans up external links

--- shared/config/base.ts
+++ shared/config/base.ts
@@ -1,1 +1,3 @@
 export const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
+export const TWITTER_HANDLE = "getrunes"
+export const DISCORD_INVITE_LINK = "https://discord.com/invite/pnx9Rjte2R"
--- src/components/ExternalLogos.tsx
+++ src/components/ExternalLogos.tsx
@@ -1,6 +1,7 @@
 import { Link, Stack, StackItem } from "@chakra-ui/react"
 import Image from "next/image"
 import React, { useMemo } from "react"
+import { DISCORD_INVITE_LINK, TWITTER_HANDLE } from "../../shared/config/base"
 import { getNetworkConfig } from "../utils/network"
 
 type LogoItemProps = {
@@ -16,7 +17,7 @@ const LogoItem: React.FC<LogoItemProps> = ({ image, href }) => {
       rel="noopener noreferrer"
       _focus={{ outline: "none" }}
     >
-      <StackItem opacity={0.6} _hover={{ opacity: 0.8 }} >
+      <StackItem opacity={0.6} _hover={{ opacity: 0.8 }}>
         <Image src={image} width={32} height={32} />
       </StackItem>
     </Link>
@@ -44,14 +45,14 @@ export const ExternalLogos = () => {
 
   return (
     <Stack direction="row" spacing={4}>
-      <LogoItem href="https://discord.com/invite/pnx9Rjte2R" image="/discord_logo.png" />
+      <LogoItem href={DISCORD_INVITE_LINK} image="/discord_logo.svg" />
       {etherscanLink && (
-        <LogoItem href={etherscanLink} image="/etherscan_logo.png" />
+        <LogoItem href={etherscanLink} image="/etherscan_logo.svg" />
       )}
       {openseaUrl && (
         <LogoItem href={openseaUrl} image="/opensea_logo.png" />
       )}
-      <LogoItem href="https://twitter.com/getrunes" image="/twitter_logo.png" />
+      <LogoItem href={`https://twitter.com/${TWITTER_HANDLE}`} image="/twitter_logo.svg" />
     </Stack>
   )
 }
